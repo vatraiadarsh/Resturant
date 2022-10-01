be2149de26d0596e05aed5c70528e2e3b63e091b
Adarsha <vatraiadarsh@gmail.com>
2022-10-01 19:27:13 +1000

    Implemented Unit of work with Repository Manager

---
+++ Repository/RepositoryManager.cs
@@ -0,0 +1,26 @@
+﻿using Contracts;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class RepositoryManager : IRepositoryManager
+    {
+
+        private readonly RepositoryContext _context;
+
+        public RepositoryManager(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        public void Dispose() => _context.Dispose();
+
+        public Task saveAsync() => _context.SaveChangesAsync();
+        
+    }
+}
